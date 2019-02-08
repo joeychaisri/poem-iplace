7ce567ad2a834edadf52c65df9fb4969ffd38700
Pathompong Chaisri <kiraboshi47@hotmail.com>
2019-02-08 15:49:48 +0700

    add directory list template

--- src/containers/directory.js
+++ src/containers/directory.js
@@ -8,53 +8,204 @@ render () {
             <div className="directory-banner">
             </div>
 
-            <div className="directory-floorplan-1">
+            <div className="directory-floorplan-1 res-img">
             </div>
                 
-            <div className="directory-floorplan-2">
+            <div className="directory-floorplan-2 res-img">
             </div>
 
             <div className="directory-container">
                 <div className="directory-container">             
                     <div className="container">
                     <div className="directory-container-head">
-                    <h3 className="directory-container-head-header"> STORE & RESTAURANT  <span style={{color:'#eb6b00'}}>DIRECTORY</span></h3>
+                        <div className="container">
+                            <div className="row">
+                                <h3 className="directory-container-head-header col-9"> STORE & RESTAURANT  <span style={{color:'#eb6b00'}}>DIRECTORY</span></h3>
+                                {/* TODO: Dropdown is here! */}
+                                <div className="sort-dropdown">
+                                    <div className=" btn-group">
+                                        <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            CATEGORY
+                                        </button>
+                                        <div className="dropdown-menu">
+                                            <a className="dropdown-item" href="#">Action</a>
+                                            <a className="dropdown-item" href="#">Another action</a>
+                                            <a className="dropdown-item" href="#">Something else here</a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="#">Separated link</a>
+                                        </div>
+                                    </div>
+                                    <div className="btn-group ml-3">
+                                        <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            FLOOR
+                                        </button>
+                                        <div className="dropdown-menu">
+                                            <a className="dropdown-item" href="#">Action</a>
+                                            <a className="dropdown-item" href="#">Another action</a>
+                                            <a className="dropdown-item" href="#">Something else here</a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="#">Separated link</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
                     </div>
-                        <div className="row">
-                            <div className="col-3 center">
-                                <div class="card">
-                                    <div class="card-body">
-                                        This is some text within a card body.
+                        <div className="row text-normal">
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
                                     </div>
                                 </div>                          
                             </div>
-                            <div className="col-3 center">
-                                <div class="card">
-                                    <div class="card-body">
-                                        This is some text within a card body.
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
                                     </div>
-                                </div>  
+                                </div>                          
                             </div>
-                            <div className="col-3 center">
-                                <div class="card">
-                                    <div class="card-body">
-                                        This is some text within a card body.
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
                                     </div>
-                                </div>  
-                            </div>
-                            <div className="col-3 center">
-                                <div class="card">
-                                    <div class="card-body">
-                                        This is some text within a card body.
+                                </div>                          
+                            </div> 
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
                                     </div>
-                                </div>  
-                            </div>                
+                                </div>                          
+                            </div>              
                         </div>
-                        
+                        <div className="row text-normal">
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div> 
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>              
+                        </div>
+                        <div className="row text-normal mb-5">
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div> 
+                            <div className="col-lg-3 col-sm-6 mt-5 center">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img className="res-img" src={require('../img/directory-logo/tops200x200.png')}  alt="fireSpot"/>
+                                        <p>ท็อปส์ มาร์เก็ต</p>
+                                        <hr style={{height:'2px', border:'none',color:'#333',backgroundColor:'#333'}} />
+                                        <p>7.00 - 23.00 น.</p>
+                                        <p>เปิดทุกวัน</p>
+                                        <p className="text-primary">G Floor</p>
+                                    </div>
+                                </div>                          
+                            </div>              
+                        </div>
+
                     </div>
                 </div>
             </div>
-
             
         </div>
     );
