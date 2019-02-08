053967e1490b0f5e687b0a37e61671d91914fcb1
Pathompong Chaisri <kiraboshi47@hotmail.com>
2019-02-08 16:15:26 +0700

    initial gallery page

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -1,12 +1,17 @@
 import React from 'react'
 class Footer extends React.Component {
-  render () {
+render () {
     return (
-        <div className="">
-        
+        <div className="gallery">
+            <div className="gallery-banner">
+            </div>
+            <div class="gallery-head">
+                <h1 class="gallery-head-header" style={{margin:'30px 30px'}}>RETAIL SPACE </h1>
+                <p className="gallery-head-paragraph">ไอเพลส พาร์ค ลาดกระบัง คอมมูนิตี้มอลล์ ศูนย์รวมร้านค้าต่างๆมากมายไว้คอยให้บริการ อำนวยความสะดวกสบาย เราใส่ใจทุกย่างก้าวที่ลูกค้าเข้ามาใช้บริการ อาทิ ซุปเปอร์มาเก็ต (ท็อป มาร์เก็ต), ร้านอาหาร (แมคโดนัล, เดอะ พิซซ่า คอมพานี, เคเอฟซี, เอสแอนด์พี, โออิชิ), ร้านกาแฟ (คาเฟ่ อเมซอน) ธนาคาร, ตู้เอทีเอม, ร้านขายยา และ ศูนย์รวมไอทีครบวงจร</p>
+            </div>            
         </div>
     );
-  }
+}
 }
 
 export default Footer;
