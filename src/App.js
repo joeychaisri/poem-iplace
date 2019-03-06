0626f1d35895596b6cd0536e20def22d5dc2912d
Pathompong Chaisri <kiraboshi47@hotmail.com>
2019-03-06 14:37:36 +0700

    add i18next library for multilanguage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component , Suspense } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 
+const Loader = () => (
+  <div className="App">
+    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <div>loading...</div>
+  </div>
+);
 class App extends Component {
   render() {
     return (
+      <Suspense fallback={<Loader />}>
       <BrowserRouter>
-        <Routes
-        />
-    </BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
+      </Suspense>
+      
     );
   }
 }
