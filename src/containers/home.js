0959ea7b8791a5587070806bb9e64f42e58f08ce
Pathompong Chaisri <kiraboshi47@hotmail.com>
2019-03-08 14:09:05 +0700

    เพิ่มคำแปลหน้า home ภาษา en / th

--- src/containers/home.js
+++ src/containers/home.js
@@ -30,7 +30,7 @@ class Home extends React.Component {
                 <span className="heading-primary--main">THE</span>
                 <span className="heading-primary--sub">OASIS IN LAT KRABANG</span>
             </h1>
-            <a className="btn btn--white btn--animated">ขอใบเสนอราคา</a>
+            <a className="btn btn--white btn--animated">{t('content.part0.quotation')}</a>
           </div>
         </div>
 
@@ -106,34 +106,33 @@ class Home extends React.Component {
         </section>
 
         <div class="information__content1">
-             <h4 class="information__content1__header heading-2">ไอเพลส</h4>
-             <h4 class="information__content1__subheader heading-2">ลาดกระบัง</h4>
+             <h4 class="information__content1__header heading-2">{t('content.part1.header')}</h4>
+             <h4 class="information__content1__subheader heading-2" style={{display:"inline"}}>{t('content.part1.subheader')}</h4>
              <div class="information__content1__box" style={{marginTop:"30px"}}>
-                <p>{t('content.part1')}</p>
-                <button onClick={() => changeLanguage('en')}>en</button>
+                <p>{t('content.part1.description')}</p>
              </div>     
         </div>
 
         <div class="information__content2">
-        <h4 class="information__content2__header heading-2">ไอเพลส</h4>
-        <h4 class="information__content2__subheader heading-2">ศูนย์การค้า</h4>
+        <h4 class="information__content2__header heading-2">{t('content.part2.header')}</h4>
+        <h4 class="information__content2__subheader heading-2">{t('content.part2.subheader')}</h4>
         <div class="information__content2__box" style={{marginTop:"30px"}}>
                 <div style={{margin:"30px 30px 30px 30px"}}>
 
-                <p class="information__content2__boxheader" >ที่อยู่:</p>
-                <p>ไอเพลส พาร์ค</p>
-                <p>เลขที่ 1 ซอยฉลองกรุง 31 แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520</p>
+                <p class="information__content2__boxheader" >{t('content.part2.boxheader')}</p>
+                <p>{t('content.part2.iplace')}</p>
+                <p>{t('content.part2.address')}</p>
                 <br/>
-                <a className="btn btn--animated" >ขอใบเสนอราคา</a>
+                <a className="btn btn--animated" >{t('content.part2.googlemap')}</a>
                 <br/>
                 <br/>
-                <p class="information__content2__boxheader">เบอร์โทรศัพท์</p>
-                <p>+66 2 360 5300-1, +66 81 947 5863</p>
+                <p class="information__content2__boxheader">{t('content.part2.tellno')}</p>
+                <p>{t('content.part2.call')}</p>
                 <br/>
-                <p class="information__content2__boxheader">เวลาเปิดปิด</p>
-                <p>เปิดทำการทุกวัน 10:00 - 22:00</p>
+                <p class="information__content2__boxheader">{t('content.part2.openclose')}</p>
+                <p>{t('content.part2.worktime')}</p>
                 <br/>
-                <a className="btn btn--animated">ขอใบเสนอราคา</a>
+                <a className="btn btn--animated">{t('content.part2.aboutus')}</a>
                 <br/>
                 
                 </div>
@@ -142,17 +141,17 @@ class Home extends React.Component {
         </div>
 
         <div class="home-travel">
-        <div style={{textAlign: 'center'}}><img src="/../img/t1.png" alt="t1" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>12.6<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>Suvarnabhumi International Airport</span></div>
-        <div style={{textAlign: 'center'}}><img src="/../img/t2.png" alt="t2" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>0.5<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>Lat Krabang Industrial Estate</span></div>
-        <div style={{textAlign: 'center'}}><img src="/../img/t3.png" alt="t3" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>12.3<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>Airport Rail Link (Lat Krabang Station)</span></div>
-        <div style={{textAlign: 'center'}}><img src="/../img/t4.png" alt="t4" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>9.5<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>Motorway no.7</span></div>
-        <div style={{textAlign: 'center'}}><img src="/../img/t5.png" alt="t5" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>6.7<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>King Mongkut's Institute of Technology Ladkrabang</span></div>
+        <div style={{textAlign: 'center'}}><img src="/../img/t1.png" alt="t1" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>12.6<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>{t('content.part3.pic1')}</span></div>
+        <div style={{textAlign: 'center'}}><img src="/../img/t2.png" alt="t2" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>0.5<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>{t('content.part3.pic2')}</span></div>
+        <div style={{textAlign: 'center'}}><img src="/../img/t3.png" alt="t3" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>12.3<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>{t('content.part3.pic3')}</span></div>
+        <div style={{textAlign: 'center'}}><img src="/../img/t4.png" alt="t4" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>9.5<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>{t('content.part3.pic4')}</span></div>
+        <div style={{textAlign: 'center'}}><img src="/../img/t5.png" alt="t5" className=""/><span style={{display:'block' , fontSize:'4rem' , fontWeight:'450'}}>6.7<span style={{color:'#eb6b00',fontSize:'2rem'}}>km</span></span><span style={{fontSize:'1.4rem'}}>{t('content.part3.pic5')}</span></div>
         </div>
 
         <div class="home_news">
-        <h1 class="home_news__header">iPLACE ข่าวสารและกิจกรรม</h1>
-        <a className="btn  btn--animated home_news__button">อ่านข่าวทั้งหมด</a>
-        <p className="home_news__no-news heading-4">No news at this time.</p>
+        <h1 class="home_news__header">{t('content.part4.newsheader')}</h1>
+        <a className="btn  btn--animated home_news__button">{t('content.part4.newsbutton')}</a>
+        <p className="home_news__no-news heading-4">{t('content.part4.news')}</p>
         </div>
 
         <div class="home-sponsor">
