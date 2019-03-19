cf38cd827e46d742049a2d54e8a747bd103243d3
Pathompong Chaisri <kiraboshi47@hotmail.com>
2019-03-19 11:18:37 +0700

    เพิ่มสองภาษา gallery + serviceretail

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -1,14 +1,19 @@
 import React from 'react'
-class Footer extends React.Component {
+import { withTranslation } from 'react-i18next';
+class Gallery extends React.Component {
 render () {
+    const { t, i18n } = this.props;
+    const changeLanguage = lng => {
+            i18n.changeLanguage(lng);
+    };
     return (
         <div className="gallery">
             <div className="gallery-banner">
             </div>
 
             <div className="gallery-head">
-                <h1 className="gallery-head-header" style={{margin:'30px 30px'}}>RETAIL SPACE </h1>
-                <p className="gallery-head-paragraph">ไอเพลส พาร์ค ลาดกระบัง คอมมูนิตี้มอลล์ ศูนย์รวมร้านค้าต่างๆมากมายไว้คอยให้บริการ อำนวยความสะดวกสบาย เราใส่ใจทุกย่างก้าวที่ลูกค้าเข้ามาใช้บริการ อาทิ ซุปเปอร์มาเก็ต (ท็อป มาร์เก็ต), ร้านอาหาร (แมคโดนัล, เดอะ พิซซ่า คอมพานี, เคเอฟซี, เอสแอนด์พี, โออิชิ), ร้านกาแฟ (คาเฟ่ อเมซอน) ธนาคาร, ตู้เอทีเอม, ร้านขายยา และ ศูนย์รวมไอทีครบวงจร</p>
+                <h1 className="gallery-head-header" style={{margin:'30px 30px'}}>{t('header')}</h1>
+                <p className="gallery-head-paragraph">{t('description')}</p>
             </div>   
 
             
@@ -110,4 +115,4 @@ render () {
 }
 }
 
-export default Footer;
+export default withTranslation('gallery')(Gallery);
--- src/containers/service-retail.js
+++ src/containers/service-retail.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import { withTranslation } from 'react-i18next';
 class serviceRetail extends React.Component {
   render () {
+    const { t, i18n } = this.props;
+    const changeLanguage = lng => {
+            i18n.changeLanguage(lng);
+    };
     return (
         <div className="service-retail">
             <div className="retail-banner">
             </div>
             <div class="retail-space">
-            <h1 class="retail-space-header" style={{margin:'30px 30px'}}>RETAIL SPACE </h1>
-            <p className="retail-space-paragraph">ไอเพลส พาร์ค ลาดกระบัง คอมมูนิตี้มอลล์ ศูนย์รวมร้านค้าต่างๆมากมายไว้คอยให้บริการ อำนวยความสะดวกสบาย เราใส่ใจทุกย่างก้าวที่ลูกค้าเข้ามาใช้บริการ อาทิ ซุปเปอร์มาเก็ต (ท็อป มาร์เก็ต), ร้านอาหาร (แมคโดนัล, เดอะ พิซซ่า คอมพานี, เคเอฟซี, เอสแอนด์พี, โออิชิ), ร้านกาแฟ (คาเฟ่ อเมซอน) ธนาคาร, ตู้เอทีเอม, ร้านขายยา และ ศูนย์รวมไอทีครบวงจร
+            <h1 class="retail-space-header" style={{margin:'30px 30px'}}>{t('header')}</h1>
+            <p className="retail-space-paragraph">{t('description')}
 </p>
-            <a className="btn  btn--animated retail-space-button" style={{margin:'0 auto', textAlign:'center'}}>ค้นหาร้านค้าทั้งหมด</a>
+            <a className="btn  btn--animated retail-space-button" style={{margin:'0 auto', textAlign:'center'}}>{t('seeallshops')}</a>
             </div>            
         </div>
     );
   }
 }
 
-export default serviceRetail;
+export default withTranslation('serviceretail')(serviceRetail);
